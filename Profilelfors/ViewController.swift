a5cd62f219076a7b2ba68e246e35c11a10056c03
KIho Mmorita <kiho.h1.chukecheese@gmail.com>
2020-09-20 23:43:33 +0900

    done

--- Profilelfors/ViewController.swift
+++ Profilelfors/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var profileImageVier: UIImageView!
+    @IBOutlet var commentLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+   
     }
 
+    @IBAction func tapbutton1(){
+        profileImageVier.image = UIImage(named: "philImage")
+        titleLabel.text = "名前"
+        commentLabel.text = "iPhoneメンターのフィルだよ"
+    }
+    
+    @IBAction func tapbutton2(){
+        profileImageVier.image = UIImage(named: "trackImage")
+        titleLabel.text = "スポーツ"
+        commentLabel.text = "陸上部に入ってて、長距離走が得意だよ"
+    }
+    
+    @IBAction func tapbutton3(){
+        profileImageVier.image = UIImage(named: "appleImage")
+        titleLabel.text = "好きな食べ物"
+        commentLabel.text = "リンゴを丸かじりするのが特に好きなんだ"
+    }
+    
+    @IBAction func tapbutton4(){
+        profileImageVier.image = UIImage(named: "flightImage")
+        titleLabel.text = "趣味"
+        commentLabel.text = "飛行機を眺めるのが好きで空港によく行くよ"
+    }
 
 }
 
